c88626b78d007d1ddf887137c78b698c209981f7
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-19 00:58:56 -0300

    upar para testes

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ let lists = document.getElementsByClassName("recipe");
 
 activate[0].addEventListener("click", function(){
 	lists[0].style.display = "flex";
+	lists[0].style.flexDirection = "column";
+	lists[0].style.width = "60%";
+	lists[0].style.alignItems = "start";
 })
