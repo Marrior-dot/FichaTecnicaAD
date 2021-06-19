83a7460ffd52d14a9b8a01808582f62a6286999f
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-19 15:02:14 -0300

    funcionalidade de mostrar e sumir receitas funcionando

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ let activate = document.getElementsByClassName("list");
 let lists = document.getElementsByClassName("recipe");
 
 activate[0].addEventListener("click", function(){
+if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
 	lists[0].style.backgroundColor = "#8C8C8C";
 	lists[0].style.margin = "0 auto";
 	lists[0].style.width = "20%";
 	lists[0].style.alignItems = "center";
+	
+}
+else{
+	lists[0].style.display = "none";
+}
+
 })
