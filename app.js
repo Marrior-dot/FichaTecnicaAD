31358b81f460bdd8dea47e189b8a030e001389dd
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-19 15:29:09 -0300

    consertando media querry

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let lists = document.getElementsByClassName("recipe");
 activate[0].addEventListener("click", function(){
 if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
-	lists[0].style.backgroundColor = "#8C8C8C";
+	lists[0].style.gridTemplateColumns = "8rem 8rem"
 	lists[0].style.margin = "0 auto";
-	lists[0].style.width = "20%";
-	lists[0].style.alignItems = "center";
+	lists[0].style.marginTop = "0.5rem";
+	lists[0].style.width = "70%";
 	
 }
 else{
