333e0443e77bccfafac258fd3bf3d9f7e554d791
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-19 14:54:49 -0300

    procurando aplciar funcionalidade de mostrar e sumir

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ let activate = document.getElementsByClassName("list");
 let lists = document.getElementsByClassName("recipe");
 
 activate[0].addEventListener("click", function(){
-	lists[0].style.display = "flex";
-	lists[0].style.flexDirection = "column";
-	lists[0].style.width = "60%";
-	lists[0].style.alignItems = "start";
+	lists[0].style.display = "grid";
+	lists[0].style.backgroundColor = "#8C8C8C";
+	lists[0].style.margin = "0 auto";
+	lists[0].style.width = "20%";
+	lists[0].style.alignItems = "center";
 })
