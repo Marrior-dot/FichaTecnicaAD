2f97c3769672e64a804dc32a4d82f7a477bcaf6e
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-30 16:45:49 -0300

    layout mais responsivo

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let lists = document.getElementsByClassName("recipe");
 activate[0].addEventListener("click", function(){
 if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
-	lists[0].style.gridTemplateColumns = "8rem 8rem"
+	lists[0].style.justifyContent = "center";
+	lists[0].style.gridTemplateColumns = "12rem 12rem 12rem 12rem";
 	lists[0].style.margin = "0 auto";
-	lists[0].style.marginTop = "0.5rem";
-	lists[0].style.width = "70%";
+	lists[0].style.width = "100%";
 	
 }
 else{
