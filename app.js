efa30d9b305b60e1b7f409092856b2a151732a42
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-30 17:50:21 -0300

    ...

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ let lists = document.getElementsByClassName("recipe");
 activate[0].addEventListener("click", function(){
 if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
-	lists[0].style.justifyContent = "center";
 	lists[0].style.gridTemplateColumns = "25% 25% 25% 25%";
-	lists[0].style.margin = "0 auto";
-	
 }
 else{
 	lists[0].style.display = "none";
