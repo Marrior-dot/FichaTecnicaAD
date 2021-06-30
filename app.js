42bcb02f84c4e644848e36cd074568a77412035c
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-30 18:57:34 -0300

    fazer grid responsivo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let tit = document.getElementById("title");
 let activate = document.getElementsByClassName("list");
 let lists = document.getElementsByClassName("recipe");
+let field = document.getElementsByTagName("fieldset");
+
 
 activate[0].addEventListener("click", function(){
 if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
 	lists[0].style.gridTemplateColumns = "25% 25% 25% 25%";
+	lists[0].style.gridTemplateAreas = "a img img a";
+	fieldset.style.width = "min(100rem, 80%)";
 }
 else{
 	lists[0].style.display = "none";
