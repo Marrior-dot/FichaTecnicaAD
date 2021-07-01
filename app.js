9283f147e616f8da2b4cb0ced80f1c895137323b
Marrior-dot <mrlimaroberto@gmail.com>
2021-07-01 17:51:15 -0300

    tamanho minimo do grid alterado para 13rem conforme o tamanho da tela

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let lists = document.getElementsByClassName("recipe");
 activate[0].addEventListener("click", function(){
 if(lists[0].style.display === "none"){
 	lists[0].style.display = "grid";
-	lists[0].style.gridTemplateColumns = "repeat(auto-fit,minmax(25%,1fr))";
+	lists[0].style.gridTemplateColumns = "repeat(auto-fit,minmax(13rem,1fr))";
 	lists[0].style.gridGap = "1rem";
 	lists[0].style.marginTop = "1rem";
-	lists[0].style.paddig = "0 1rem";
+	lists[0].style.paddig = "0 rem";
 }
 else{
 	lists[0].style.display = "none";
